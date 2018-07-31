8c4aeb8a129c6a681b0e363e41a96def64198416
Natalie Roode <35185143+nroode@users.noreply.github.com>
2018-07-30 22:00:30 -0700

    Add files via upload

---
+++ QuoteJs.js
@@ -0,0 +1,28 @@
+
+
+    
+$(getQuote);
+
+$('#button').on('click', getQuote);
+
+
+
+function getQuote() {
+    quoteUrl = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+    
+    $.getJSON(quoteUrl, function (response) {
+
+       
+        $('#quote').html(response.quoteText);
+
+        $('#author').html(response.quoteAuthor);
+
+    });
+};
+
+
+$('.twitter-share-button').on('click', function() {
+    var tweetQuote = $('#quoteText').text();
+    window.open("https://twitter.com/intent/tweet?text=" + tweetQuote);
+});
+
